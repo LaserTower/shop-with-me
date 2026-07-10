5ab3b91ce91a68a7ddf32871e662b30e7d6d3bf1
Александр Андреевич <LaserTower@gmail.com>
2026-07-10 13:52:38 +0500

    add link

--- app/pages/+Layout.tsx
+++ app/pages/+Layout.tsx
@@ -17,6 +17,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <Link href="/">Welcome</Link>
         <Link href="/todo">Todo</Link>
         <Link href="/star-wars">Data Fetching</Link>
+        <Link href="/test">Tailwind Examples</Link>
       </Sidebar>
       <Content>{children}</Content>
     </div>
